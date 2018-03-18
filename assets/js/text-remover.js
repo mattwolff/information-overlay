cb16211f5b3732ca00a3815da6da9a9c3a8aa15f
Matt Wolff <matt.t.wolff@gmail.com>
2018-03-18 15:36:14 -0400

    transparent color instead of zero opacity

--- assets/js/text-remover.js
+++ assets/js/text-remover.js
@@ -31,7 +31,8 @@
 		(window.bookmarklet = function() {
 			// Replace this section with your own Information Overlay script
 			$("body").contents().filter(function() {return this.nodeType === 3;}).wrap("<p></p>").end();
-			$("h1, h2, h3, h4, h5, h6, p, ul, ol, pre, span, strong, b, em, a, mark, i").css("opacity", "0");
+			$("h1, h2, h3, h4, h5, h6, p, ul, ol, pre, span, strong, b, em, a, mark, i").css("color", "rgba(0,0,0,0)");
+			$("*").css("color", "rgba(0,0,0,0)");
 			$("img").css("opacity", "1");
 			$("svg").css("opacity", "1");
 		})();
